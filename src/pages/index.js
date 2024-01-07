bc60d5752d633ef40cd012c7b4e8d26180b1b8dc
grabdoc <grabdoc2020@gmail.com>
2024-01-07 14:22:43 -0600

    updated docs

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,8 +32,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="Instant REST Data API. Boost development time by 30x <head />">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
