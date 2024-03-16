2bfefa0e7fe098ca22edd6bc2926500e6dac3663
grabdoc <grabdoc2020@gmail.com>
2024-03-16 11:33:49 -0500

    tembo application service

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -146,15 +146,16 @@ const config = {
         additionalLanguages: ['java'],
       },
 
+      /*
       announcementBar: {
         id: 'support_us',
         content:
-            'We are looking to revamp our docs, please fill <a target="_blank" rel="noopener noreferrer" href="#">this survey</a>',
-        backgroundColor: '#fafbfc',
+            'We are updating our documentation <a target="_blank" rel="noopener noreferrer" href="#">this survey</a>',
+        backgroundColor: '#DAF7A6',
         textColor: '#091E42',
         isCloseable: false,
       },
-
+      */
 
     }),
 };
