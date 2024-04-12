774af232201cdd90217c79024493b7533f1d816c
Thad Guidry <thadguidry@gmail.com>
2024-04-12 21:06:51 +0800

    Fix url in Docusaurus config
    - We shouldn't have included the `baseUrl` string part at the end of the `url`, so removing, which hopefully "just fixes" the Algolia search ignored 104 pages issue after a new crawl !!!

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   favicon: 'img/favicon.svg',
 
   // Set the production url of your site here
-  url: 'https://db2rest.com/',
+  url: 'https://db2rest.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
